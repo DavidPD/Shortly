5742afd1feb4192c73a6894aa8ec53f6b7116ac9
David Dovel <davey5555@gmail.com>
2019-08-31 11:41:34 -0500

    Update POST endpoint to match requirements

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/', action: 'index', controller: 'shortened_url'
-  post '/', action: 'create', controller: 'shortened_url'
+  post '/short_link', action: 'create', controller: 'shortened_url'
   get '/*short_hash', action: 'show', controller: 'shortened_url'
 end
